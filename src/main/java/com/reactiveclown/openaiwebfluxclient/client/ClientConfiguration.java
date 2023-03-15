bd9e9af8c27726775a5af18d1e75a2d88e7fa518
Reactive clown <clown.reactive@gmail.com>
2023-03-15 18:03:52 +0200

    CompletionsServiceImpl and ModelsServiceImpl implementation.

--- src/main/java/com/reactiveclown/openaiwebfluxclient/client/ClientConfiguration.java
+++ src/main/java/com/reactiveclown/openaiwebfluxclient/client/ClientConfiguration.java
@@ -1,21 +1,24 @@
 package com.reactiveclown.openaiwebfluxclient.client;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
-public class AsynchronousConfiguration {
+public class ClientConfiguration {
+
+    @Value("${openai.api.baseUrl:https://api.openai.com/v1}")
+    private String baseUrl;
+
+    @Value("${openai.api.key}")
+    private String apiKey;
 
     @Bean
     public WebClient webClient() {
-        String baseUrl = "https://api.openai.com/v1";
         return WebClient.builder()
                 .baseUrl(baseUrl)
-                .defaultHeader("Authorization", "Bearer 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")
-                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .defaultHeader("Authorization", String.format("Bearer %s", apiKey))
                 .build();
     }
 }
