48259146a59d722363fb15ce57f39a14f83cd88e
Reactive clown <clown.reactive@gmail.com>
2023-03-16 13:18:45 +0200

    Adding organizationId as not required header.

--- src/main/java/com/reactiveclown/openaiwebfluxclient/client/ClientConfiguration.java
+++ src/main/java/com/reactiveclown/openaiwebfluxclient/client/ClientConfiguration.java
@@ -3,6 +3,7 @@ package com.reactiveclown.openaiwebfluxclient.client;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
@@ -14,11 +15,24 @@ public class ClientConfiguration {
     @Value("${openai.api.key}")
     private String apiKey;
 
+    @Value("${opeanai.api.organizationId:#{null}}")
+    private String organizationId;
+
     @Bean
     public WebClient webClient() {
+        if (organizationId == null) {
+            return WebClient.builder()
+                    .baseUrl(baseUrl)
+                    .defaultHeader(HttpHeaders.AUTHORIZATION, String.format("Bearer %s", apiKey))
+                    .build();
+        }
+
         return WebClient.builder()
                 .baseUrl(baseUrl)
-                .defaultHeader("Authorization", String.format("Bearer %s", apiKey))
+                .defaultHeaders(httpHeaders -> {
+                    httpHeaders.add(HttpHeaders.AUTHORIZATION, String.format("Bearer %s", apiKey));
+                    httpHeaders.add("OpenAI-Organization", organizationId);
+                })
                 .build();
     }
 }
