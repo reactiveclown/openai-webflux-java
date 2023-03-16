aef868c4707caa7a9260eb9e457ccd7527eede7c
Reactive clown <clown.reactive@gmail.com>
2023-03-16 13:01:52 +0200

    ChatServiceImpl implementation.

---
+++ src/main/java/com/reactiveclown/openaiwebfluxclient/client/chat/ChatServiceImpl.java
@@ -0,0 +1,23 @@
+package com.reactiveclown.openaiwebfluxclient.client.chat;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+@Service
+public class ChatServiceImpl implements ChatService{
+
+    private final WebClient client;
+    public ChatServiceImpl(WebClient client){
+        this.client = client;
+    }
+
+    @Override
+    public Mono<CreateChatCompletionResponse> createChatCompletion(CreateChatCompletionRequest request) {
+        String createChatCompletionUrl = "/completions";
+        return client.post()
+                .uri(createChatCompletionUrl)
+                .retrieve()
+                .bodyToMono(CreateChatCompletionResponse.class);
+    }
+}
