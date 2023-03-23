1ab7b3dd25dc8c7d16480d20d7e329ad288c8d37
Reactive clown <clown.reactive@gmail.com>
2023-03-23 12:20:37 +0200

    CreateImageRequest JavaDocs added and refactoring.

--- src/main/java/com/reactiveclown/openaiwebfluxclient/client/images/CreateImageRequest.java
+++ src/main/java/com/reactiveclown/openaiwebfluxclient/client/images/CreateImageRequest.java
@@ -1,31 +1,36 @@
 package com.reactiveclown.openaiwebfluxclient.client.images;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+/**
+ * CreateImageRequest.
+ *
+ * @param prompt         - A text description of the desired image(s).
+ *                       The maximum length is 1000 characters.
+ * @param n              - The number of images to generate.
+ *                       Must be between 1 and 10.
+ * @param size           - The size of the generated images.
+ *                       Must be one of 256x256, 512x512, or 1024x1024.
+ * @param responseFormat - The format in which the generated images are returned.
+ *                       Must be one of url or b64_json.
+ * @param user           - A unique identifier representing your end-user,
+ *                       which can help OpenAI to monitor and detect abuse.
+ */
+@JsonInclude(Include.NON_NULL)
 public record CreateImageRequest(@JsonProperty("prompt") String prompt,
                                  @JsonProperty("n") Integer n,
                                  @JsonProperty("size") String size,
                                  @JsonProperty("response_format") String responseFormat,
                                  @JsonProperty("user") String user) {
     public CreateImageRequest {
-        //Providing the default values
-        if (n == null) n = 1;
-        if (size == null) size = "1024x1024";
-        if (responseFormat == null) responseFormat = "url";
-        if (user == null) user = "";
-
-        //Checking the restrictions
         if (prompt == null || prompt.isEmpty())
             throw new IllegalArgumentException("prompt value can't be null or empty");
+    }
 
-        if (n > 10 || n < 1)
-            throw new IllegalArgumentException("n value should be between [1, 10]");
-
-        if (!size.equals("1024x1024") && !size.equals("256x256") && !size.equals("512x512"))
-            throw new IllegalArgumentException("size value must be one of \"256x256\", \"512x512\", or \"1024x1024\"");
-
-        if (!responseFormat.equals("url") && !responseFormat.equals("b64_json"))
-            throw new IllegalArgumentException("responseFormat value must be one of \"url\" or \"b64_json\"");
+    public static Builder builder(String prompt) {
+        return new Builder(prompt);
     }
 
     public static final class Builder {
@@ -41,7 +46,7 @@ public record CreateImageRequest(@JsonProperty("prompt") String prompt,
                     responseFormat, user);
         }
 
-        public Builder(String prompt){
+        public Builder(String prompt) {
             this.prompt = prompt;
         }
 
