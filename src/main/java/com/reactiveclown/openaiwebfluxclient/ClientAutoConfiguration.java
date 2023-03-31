b12e0f02d2754a5c35ba7ab777784436dd989790
Reactive clown <clown.reactive@gmail.com>
2023-03-31 12:06:58 +0300

    Basic exception handling added.

--- src/main/java/com/reactiveclown/openaiwebfluxclient/ClientAutoConfiguration.java
+++ src/main/java/com/reactiveclown/openaiwebfluxclient/ClientAutoConfiguration.java
@@ -6,18 +6,22 @@ import com.reactiveclown.openaiwebfluxclient.client.images.ImageServiceImpl;
 import com.reactiveclown.openaiwebfluxclient.client.images.ImagesService;
 import com.reactiveclown.openaiwebfluxclient.client.models.ModelsService;
 import com.reactiveclown.openaiwebfluxclient.client.models.ModelsServiceImpl;
+import com.reactiveclown.openaiwebfluxclient.exception.OpenAiException;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
+import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 @Configuration
 @AutoConfiguration
 @EnableConfigurationProperties(ClientAutoConfigurationProperties.class)
 public class ClientAutoConfiguration {
+
 
     private final ClientAutoConfigurationProperties properties;
 
@@ -25,33 +29,41 @@ public class ClientAutoConfiguration {
         this.properties = properties;
     }
 
-    @Bean
-    public WebClient webClient() {
-        if (properties.organizationId() == null) {
-            return WebClient.builder()
-                    .baseUrl(properties.baseUrl())
-                    .defaultHeader(HttpHeaders.AUTHORIZATION, String.format("Bearer %s", properties.apiKey()))
-                    .build();
-        }
-
-        return WebClient.builder()
-                .baseUrl(properties.baseUrl())
-                .defaultHeaders(httpHeaders -> {
-                    httpHeaders.add(HttpHeaders.AUTHORIZATION, String.format("Bearer %s",properties.apiKey()));
-                    httpHeaders.add("OpenAI-Organization", properties.organizationId());
-                })
-                .build();
+    public ExchangeFilterFunction errorHandlerExchangeFilterFunction() {
+        return ExchangeFilterFunction.ofResponseProcessor(clientResponse -> {
+            if (clientResponse.statusCode().isError()) {
+                return clientResponse.bodyToMono(String.class)
+                        .flatMap(errorMessage -> Mono.error(
+                                new OpenAiException(clientResponse.statusCode().value(),
+                                        errorMessage)));
+            }
+            return Mono.just(clientResponse);
+        });
     }
 
     @Bean
+    public WebClient webClient() {
+        return WebClient.builder()
+                .baseUrl(properties.baseUrl())
+                .defaultHeaders(httpHeaders -> {
+                    httpHeaders.add(HttpHeaders.AUTHORIZATION, String.format("Bearer %s", properties.apiKey()));
+                    if (properties.organizationId() != null)
+                        httpHeaders.add("OpenAI-Organization", properties.organizationId());
+                })
+                .filter(errorHandlerExchangeFilterFunction())
+                .build();
+    }
+
+
+    @Bean
     @ConditionalOnMissingBean
-    public ImagesService imagesService(WebClient client){
+    public ImagesService imagesService(WebClient client) {
         return new ImageServiceImpl(client);
     }
 
     @Bean
     @ConditionalOnMissingBean
-    public CompletionsService completionsService(WebClient client){
+    public CompletionsService completionsService(WebClient client) {
         return new CompletionsServiceImpl(client);
     }
 
---
+++ src/main/java/com/reactiveclown/openaiwebfluxclient/exception/OpenAiException.java
@@ -0,0 +1,16 @@
+package com.reactiveclown.openaiwebfluxclient.exception;
+
+import org.springframework.web.reactive.function.client.ClientResponse;
+import org.springframework.web.reactive.function.client.WebClientException;
+
+public class OpenAiException extends WebClientException {
+    public OpenAiException(Integer statusCode, String errorMessage) {
+        super(String.format("""
+                        OpenAi Service exception occurred.
+                        HTTP error code: %s
+                        Detailed error message:
+                        %s
+                        """,
+                statusCode, errorMessage));
+    }
+}
